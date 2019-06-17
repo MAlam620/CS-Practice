eb42e4185ca6476bf34c2e2bfccf458ca8f64b13
Musa Alam <mq1@umbc.edu>
2019-06-17 17:00:36 -0400

    Problems solved match what's in LeetCode profile

--- Data Structures/Arrays_Strings/Practice Problems/ReverseString.java
+++ Data Structures/Arrays_Strings/Practice Problems/ReverseString.java
@@ -1,39 +1,52 @@
 /**
- * Create a function that reverses a string:
- * "Hi My name is Andrei" should be:
- * "ierdnA si eman yM iH"
+ *Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+
+ 
+
+Example 1:
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+o, e, l, l, h
+o, l, l, e, h
+
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
  */
 
  public class ReverseString
  {
+    
     /**
-     * Detail: Time complexity is O(n), since the input
-     * is looped over once. Space complexity is O(n), since
-     * a new string is created with length equal to the input.
+     1). Create a start index variable. Iterate backwards from the end. 
+     2). Switch the value of the end variable with the start index.
+     3). End variable will update with loop. Increment start variable manually.
+     4). Kill loop when start and end variable cross each other.
      */
-    public static String reverse(String input)
+    public void reverseString(char[] s) 
     {
-        //Check for null, or single letter string
-        if(input.length() < 1)
+        /**
+         * Time complexity is O(n) since all characters of the input array are analyzed and switched.
+         * Space Complexity is O(1), since no new array is created, per the prompt.
+         */
+        int startIndex = 0;
+        for(int i = s.length - 1; i >= 0; i--)
         {
-            return "Please give me a valid string.";
+            if(startIndex >= i)
+            {
+                break;
+            }
+            char temp = s[i];
+            s[i] = s[startIndex];
+            s[startIndex] = temp;
+            startIndex++;
         }
-        else if(input.length() == 1)
-        {
-            return input;
-        }
-        String returnString = "";
-
-        for(int i = input.length() - 1; i >= 0; i--)
-        {
-            returnString += input.charAt(i);
-        }
-        return returnString;
-    }
-
-    public static void main(String [] args){
-        String test = "Hi My name is Andrei";
-        String newString = reverse(test);
-        System.out.print(newString);
     }
  }
