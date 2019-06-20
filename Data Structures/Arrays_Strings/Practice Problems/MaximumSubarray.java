9f1166dae4dd685f2d9917d33a3b37bfab6ba78e
MAlam620 <41587507+MAlam620@users.noreply.github.com>
2019-06-20 10:19:32 -0400

    Uses Dynamic Programming, revisit later.

--- Data Structures/Arrays_Strings/Practice Problems/MaximumSubarray.java
+++ Data Structures/Arrays_Strings/Practice Problems/MaximumSubarray.java
@@ -1,67 +1,56 @@
 /**
- * Given an array of numbers, find the maximum sum of any contiguous
- * subarray of the array. For example, given the array [34, -50, 42, 14, -5, 86]
- * the maximum sum would be 137, since we would take elements 42, 14, -5, and 86. Given the
- * array [-5, -1, -8, -9], the maximum sum would be 0, since we choose not to take 
- * any elements. 
- * 
- * Do this in O(n) time.
- * 
- * Follow-up: What if the elements can wrap around? FOr example, given
- * [8, -1, 3, 4], return 15, as we choose the numbers 3, 4, and 8 where the 8
- * is obtained from wrapping around.
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
  */
 
  /**
-  * Algorithm
-  1). Iterate over the array and find the indices of all positive values.
-  2). 
+  * The algorithm below is called Kadane's Algorithm, and runs in O(n), using Dynamic Programming techniques. Revisit this problem after
+  going through that section to better understand it.
   */
-
 import java.util.ArrayList;
- public class MaximumSubarray
- {
-    public int maxSubArray(int[] nums)
+public class MaximumSubarray
+{
+    public int maxSubArray(int[] A) 
     {
-       ArrayList<Integer> indices = new ArrayList<Integer>();
-
-       if(nums.length == 0)
-       {
-           return 0;
-       }
-       int minimumValue = nums[0];
-       for(int i = 0; i < nums.length; i++)
-       {
-           if(nums[i] >= 0)
-           {
-               indices.add(i);
-           }
-           minimumValue = Math.min(minimumValue, nums[0]);
-       }
-       System.out.println(indices.size() + ":)");
-       if(indices.size() <= 1)
-       {
-           return minimumValue;
-       }
-       
-       
-       int totalSum = indices.get(0);     
-       int currentSum = 0;
-       for(int i = indices.get(0); i < indices.get(indices.size()-1); i++)
-       {
-           currentSum = currentSum + nums[i];
-           currentSum = Math.max(currentSum, 0);
-           totalSum = Math.max(totalSum, currentSum);
-       }
-       
-       return totalSum;   
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < A.length; i++)
+        {
+            if (sum < 0)
+            {
+                sum = A[i];
+            } 
+                
+            else
+            {
+                sum += A[i];
+            } 
+                
+            if (sum > max)
+            {
+                max = sum;
+            }
+               
+        }
+        return max;
     }
 
     public static void main(String[] args)
     {
         MaximumSubarray test = new MaximumSubarray();
-        int[] array = {-2,1};
-        int dummy = test.maxSubArray(array);
-        System.out.println(dummy);
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+
+        int sum = test.maxSubArray(nums);
+        System.out.println(sum);
     }
- }
+}
