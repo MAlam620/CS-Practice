a436057e39a3a6fe38d0c3bd7063c4962acacc32
sdfsdf asfsdf <mq1@umbc.edu>
2019-11-16 18:09:35 -0800

    Selection sort done

---
+++ Algorithms/Sorting/SortingExercises/src/InsertionSort.java
@@ -0,0 +1,33 @@
+/**
+ * This sort is good when the list is already almost sorted. You can almost
+ * get linear time for this. The strategy is the same one you use when sorting
+ * cards in your hand.
+ *
+ * 1). Work left to right.
+ * 2). Examine each item and compare it to the items on the left.
+ * 3). Insert the item in the correct position in the array.
+ */
+public class InsertionSort
+{
+    public static int[] insertionSort(int[] array)
+    {
+        for(int i = 0; i < array.length; i++)
+        {
+            int key = array[i];
+            int j = i - 1;
+
+            /*
+            Move elements of array[0...i - 1], that are greater than the key,
+            to one position ahead of their current position
+             */
+
+            while (j >= 0 && array[j] > key)
+            {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+        return array;
+    }
+}
---
+++ Algorithms/Sorting/SortingExercises/src/SelectionSort.java
@@ -0,0 +1,45 @@
+/**
+ * You keep track of two indices. One keeps track of the minimum value so far in the data,
+ * and the second is what index you are through your iteration. You go through the entire
+ * array, and swap the position of the first element with the position of the minimal value.
+ * You do this over and over again through the array, and get the array sorted. Again, this is
+ * a nested loop, so time complexity is O(n^2).
+ */
+
+import java.lang.*;
+public class SelectionSort
+{
+    public static int[] selectionSort(int[] input)
+    {
+        int length = input.length;
+        for(int i = 0; i < length; i++)
+        {
+            int minIndex = i;
+            for(int j = i; j < length; j++)
+            {
+                if(input[j] < input[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+            int temp = input[i];
+            input[i] = input[minIndex];
+            input[minIndex] = temp;
+        }
+        return input;
+    }
+
+    public static void printArray(int[] array)
+    {
+        for(int i = 0; i < array.length; i++)
+        {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int[] input ={99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
+        printArray(selectionSort(input));
+    }
+}
